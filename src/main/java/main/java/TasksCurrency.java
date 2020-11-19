7a15040316cae6a87e1ad9056b574630f9ea1d97
Garrett Holly <HypnoticGMAN@gmail.com>
2020-11-19 14:21:57 -0700

    Created default values for different income types

---
+++ src/main/java/main/java/TasksCurrency.java
@@ -0,0 +1,24 @@
+package main.java;
+
+public class TasksCurrency {
+    private final double chickenIncome = 1.0;
+    private final double cowIncome = 5.0;
+    private final double sheepIncome = 8.0;
+    private final double cropIncome = 30.0;
+
+    public double getChickenIncome() {
+        return chickenIncome;
+    }
+
+    public double getCowIncome() {
+        return cowIncome;
+    }
+
+    public double getSheepIncome() {
+        return sheepIncome;
+    }
+
+    public double getCropIncome() {
+        return cropIncome;
+    }
+}
