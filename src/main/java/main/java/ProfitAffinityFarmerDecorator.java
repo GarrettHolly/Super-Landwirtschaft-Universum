fdc71f56753fc963a880e944cdc9bc173a580fe5
Garrett Holly <HypnoticGMAN@gmail.com>
2020-11-19 14:08:29 -0700

    Completed ProfitAffinityFarmerDecorator

--- src/main/java/main/java/ProfitAffinityFarmerDecorator.java
+++ src/main/java/main/java/ProfitAffinityFarmerDecorator.java
@@ -1,5 +1,14 @@
 package main.java;
 
-public class ProfitAffinityFarmerDecorator {
+public class ProfitAffinityFarmerDecorator extends FarmerDecorator {
+    private final double affinityMultiplier = 2 + 1.5;
 
+    public ProfitAffinityFarmerDecorator(Farmer decoratedFarmer) {
+        super(decoratedFarmer);
+    }
+
+    @Override
+    public double profitAffinity() {
+        return decoratedFarmer.husbandryAffinity() * affinityMultiplier;
+    }
 }
