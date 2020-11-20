47c510a41a4b0df5d7351a3b5f50249c6c656da4
Garrett Holly <HypnoticGMAN@gmail.com>
2020-11-19 18:17:41 -0700

    Changed Class name

---
+++ src/main/java/main/java/GameLoop.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class GameLoop {
+    // private final double animalPredatorChance = 0.1;
+    // private final double cropWitherChance = 0.4;
+    // private final double cropPredatorChance = 0.1;
+    // private final double farmerTreatCropChance = 0.7;
+    // private double animalAge = 1;
+
+}
